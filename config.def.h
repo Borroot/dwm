7487b234e2b59a47a7386924d33ce7b1baad7706
Borroot <borroot@live.nl>
2020-05-10 14:04:47 +0200

    Let the blueman manager always float.

--- config.def.h
+++ config.def.h
@@ -31,11 +31,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "discord",   NULL,      NULL,       1 << 5,       0,           -1 },
-	{ "matplotlib",NULL,      NULL,            0,       1,           -1 },
-	{ "Peek",      NULL,      NULL,            0,       1,           -1 },
-	{ "tk",        NULL,      NULL,            0,       1,           -1 },
+	/* class              instance  title  tags mask  isfloating  monitor */
+	{ "discord",          NULL,     NULL,  1 << 5,    0,          -1 },
+	{ "matplotlib",       NULL,     NULL,  0,         1,          -1 },
+	{ "Peek",             NULL,     NULL,  0,         1,          -1 },
+	{ "tk",               NULL,     NULL,  0,         1,          -1 },
+	{ "blueman-manager",  NULL,     NULL,  0,         1,          -1 },
 };
 
 /* layout(s) */
