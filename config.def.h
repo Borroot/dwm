90d5bdb8e4b544d9069fa5b277a76aceda11ec13
Borroot <borroot@live.nl>
2021-02-09 09:42:04 +0100

    Add pulsemixer rule and alwayscenter patch.

--- config.def.h
+++ config.def.h
@@ -31,12 +31,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class              instance  title  tags mask  isfloating  monitor */
-	{ "discord",          NULL,     NULL,  1 << 5,    0,          -1 },
-	{ "matplotlib",       NULL,     NULL,  0,         1,          -1 },
-	{ "Peek",             NULL,     NULL,  0,         1,          -1 },
-	{ "Tk",               NULL,     NULL,  0,         1,          -1 },
-	{ "Blueman-manager",  NULL,     NULL,  0,         1,          -1 },
+	/* class              instance      title  tags mask  isfloating  monitor */
+	{ "discord",          NULL,         NULL,  1 << 5,    0,          -1 },
+	{ "matplotlib",       NULL,         NULL,  0,         1,          -1 },
+	{ "Peek",             NULL,         NULL,  0,         1,          -1 },
+	{ "Tk",               NULL,         NULL,  0,         1,          -1 },
+	{ "Blueman-manager",  NULL,         NULL,  0,         1,          -1 },
+	{ NULL,               "pulsemixer", NULL,  0,         1,          -1 },
 };
 
 /* layout(s) */
