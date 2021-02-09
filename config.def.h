8613179e9bb5cefdecbba845f0ef829090dc995c
Borroot <borroot@live.nl>
2021-02-09 12:32:29 +0100

    Remove more unused shortcuts.

--- config.def.h
+++ config.def.h
@@ -74,15 +74,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	/*{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },*/
-	/*{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },*/
+	{ MODKEY,                       XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
