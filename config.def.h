9c7b6c576039fb974fa7863a14974674ce19a03e
Borroot <borroot@live.nl>
2021-02-09 12:27:46 +0100

    Remove unused (command) shortcuts.

--- config.def.h
+++ config.def.h
@@ -60,17 +60,11 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+/* command example */
+/* static const char *termcmd[]  = { "alacritty", "-e", "somecommand", NULL }; */
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	/*{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },*/
 	/*{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },*/
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -90,16 +84,11 @@ static Key keys[] = {
 	/*{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },*/
 	/*{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },*/
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	/*{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },*/
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	/*{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },*/
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	/*TAGKEYS(                        XK_5,                      4)*/
-	/*TAGKEYS(                        XK_6,                      5)*/
-	/*TAGKEYS(                        XK_7,                      6)*/
 	TAGKEYS(                        XK_8,                      4)
 	TAGKEYS(                        XK_9,                      5)
 	TAGKEYS(                        XK_0,                      6)
@@ -114,7 +103,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button1,        togglewin,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	/*{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },*/
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
